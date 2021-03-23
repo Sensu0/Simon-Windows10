928a2cd46dc01ef3ff2c17253550a81100ef7bee
Simon Brännström <simon.brannstrom1@gmail.com>
2021-03-23 01:25:49 +0100

    Patched. Tested it earlier and it didn't work. Dound that this works however.

--- Clean-recyclebin.ps1
+++ Clean-recyclebin.ps1
@@ -1,6 +1,14 @@
+# Copyright (C) 2021 Simon Brännström
+# GNU GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
+# Remove all files in Recycle Bin for all users.
+Write-Host "This will delete all files and folders in Recycle Bin for all users on this computer.
+
+There is no way to recover from this!"
+pause
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
 }
-Set-Location 'C:\$Recycle.Bin'
-Get-ChildItem -Name .\S-1-5-21-*\ -Exclude desktop.ini -Recurse -Attributes Archive, Compressed, Directory, Encrypted, Hidden, IntegrityStream, Normal, NoScrubData, NotContentIndexed, Offline, ReadOnly, ReparsePoint, SparseFile, Temporary | Remove-Item *  -Force
+Clear-RecycleBin -Force;
+Get-ChildItem 'C:\$Recycle.bin\' -Force | Remove-Item -Recurse -force
