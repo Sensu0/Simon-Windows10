b5b66e59dc930cbbbb3d5a5eb09c3aaf04ad2a92
Simon Brännström <simon.brannstrom1@gmail.com>
2023-04-28 21:28:41 +0200

    Added powershell script that installs a lot of software which I use via winget

---
+++ Customization/winget install.ps1
@@ -0,0 +1,46 @@
+# Tools
+winget install bitwarden --source msstore --accept-source-agreements
+winget install tidal --source msstore --accept-source-agreements
+winget install "snipping tool" --source msstore --accept-source-agreements
+winget install outlook --source msstore --accept-source-agreements
+winget install slack --source msstore --accept-source-agreements
+winget install "visual studio code" --source msstore --accept-source-agreements
+winget install "visual studio community 2022" --source msstore --accept-source-agreements
+winget install "kde connect" --source msstore --accept-source-agreements
+winget install okular --source msstore --accept-source-agreements
+winget install vlc --source msstore --accept-source-agreements
+winget install "hp smart" --source msstore --accept-source-agreements
+winget install "microsoft teams" --source msstore --accept-source-agreements
+winget install itunes --source msstore --accept-source-agreements
+winget install "brave browser" --source msstore --accept-source-agreements
+winget install 7-zip --source winget --accept-source-agreements
+winget install imgburn --source winget --accept-source-agreements
+winget install nordvpn --source winget --accept-source-agreements
+winget install virtualbox --source winget --accept-source-agreements
+winget install makemkv --source winget --accept-source-agreements
+
+# 64-bit Visual C++ (needed to make older software compatible with newer Windows OS)
+winget install "Microsoft Visual C++ 2005 Redistributable (x64)" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2010 x64 Redistributable" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2012 Redistributable (x64)" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2013 Redistributable (x64)" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2015-2022 Redistributable (x64)" --source winget --accept-source-agreements
+# 32-bit Visual C++ (needed to make older software compatible with newer Windows OS)
+
+winget install "Microsoft Visual C++ 2005 Redistributable" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2010 x86 Redistributable" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2012 Redistributable (x86)" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2013 Redistributable (x86)" --source winget --accept-source-agreements
+winget install "Microsoft Visual C++ 2015-2022 Redistributable (x86)" --source winget --accept-source-agreements
+
+# Social
+winget install whatsapp --source msstore --accept-source-agreements
+
+# Gaming
+winget install "epic games store" --source msstore --accept-source-agreements
+winget install "ubisoft connect" --source msstore --accept-source-agreements
+winget install discord --source msstore --accept-source-agreements
+winget install "EA app" --source winget --accept-source-agreements
+winget install steam --source winget --accept-source-agreements
+winget install "gog galaxy" --source winget --accept-source-agreements
+winget install "gamebar browser" --source msstore --accept-source-agreements
